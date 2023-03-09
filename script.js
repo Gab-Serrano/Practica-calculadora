961bca2b1b39ff8ccdcc3017d0269a124e661660
Gabriel Serrano <gab.serrano@duocuc.cl>
2023-03-09 02:12:21 -0300

    Agrega funcion calcular

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+const display = document.getElementById('display');
+
 const btnAC = document.getElementById('AC');
 const borrarTodo = () => {
-    const display = document.getElementById('display');
     display.value = "";
 };
 btnAC.addEventListener("click", borrarTodo);
 
-
-
-
+const btnC = document.getElementById('C');
+const borrarNum = () => {
+    display.value = display.value.toString().slice(0,-1);
+}
+btnC.addEventListener("click", borrarNum);
 
 const btnIgual = document.getElementById('igual');
+const calcular = () => {
+    display.value = eval(display.value);
+}
+btnIgual.addEventListener("click", calcular);
